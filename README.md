91a80cd5f03eb40264637c24c9efdf8a7edf0c50
Asante-ntiro <asante.ntiro@gmail.com>
2023-07-20 21:23:50 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Sample Foundry Project
+
+This project demonstrates a basic Foundry use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+forge build
+```
+Remember to create an env file with PRIVATE_KEY and CONTRACT_ADDRESS
+to run the contract on mara testnet use the command
+
+the mara explorer can be found on https://testnet.mara.xyz/
+
---
+++ scripts/calls.js
@@ -0,0 +1,35 @@
+const hre = require("hardhat");
+
+const { Contract } = require("hardhat/internal/hardhat-network/stack-traces/model");
+const contractAddress =process.env.CONTRACT_ADDRESS
+const contractName ="Mara";
+async function main(){
+
+    const call =  await hre.ethers.getContractAt(contractName,contractAddress);
+
+    const tx =  await call.addCount( 10);
+    
+
+    const receipt = await tx.wait();
+
+    const addmember =await call.creatMember(10,"mii");
+    
+    const addStudent = await  call.createStudent(11,"kiboko","0xFf897981d299e1668C9B0CfbE693A93549d92c26")
+
+
+    const  Count = await call.getCount;
+    // console.log(addmember);
+    // console.log(Count);
+
+    // console.log(receipt);
+
+     console.log(addStudent);
+
+
+
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
